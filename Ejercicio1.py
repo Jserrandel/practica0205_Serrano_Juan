f2e63b1f643891fe52bfa2e48e709d869a609ffc
Jserrandel <jserrandel@educacion.navarra.es>
2024-11-11 10:14:24 +0100

    Ejercicio 1

---
+++ Ejercicio1.py
@@ -0,0 +1,11 @@
+#Escribir una función a la que se le pase una cadena
+#<nombre> y muestre por pantalla el saludo ¡hola <nombre>!.
+def Nombre(Nombre):
+    """Función que muestra un saludo por pantalla.
+    Parámetros
+    nombre: Nombre del usuario
+    Devuelve el saludo ¡Hola nombre!.
+    """
+    print('¡Hola ' + Nombre +'!')
+Nombre("Juan")
+Nombre("Santi")
